38c5c131822ddf344f033ead0a7b8157f7c873f7
backey1995 <kontonmao@gmail.com>
2025-10-29 17:57:06 +0100

    Update spacex-dash-app.py

--- IBMDataScience_capstone/spacex-dash-app.py
+++ IBMDataScience_capstone/spacex-dash-app.py
@@ -62,6 +62,8 @@ app.layout = html.Div(children=[html.H1('SpaceX Launch Records Dashboard',
               Input(component_id='site-dropdown', component_property='value'))
 def get_pie_chart(entered_site):
     filtered_df = spacex_df[spacex_df['Launch Site'] == entered_site]
+    filtered_df = filtered_df['class'].value_counts().reset_index()
+    filtered_df.columns = ['class', 'count']
     if entered_site == 'ALL':
         fig = px.pie(spacex_df, values='class', 
         names='Launch Site', 
@@ -69,7 +71,7 @@ def get_pie_chart(entered_site):
         return fig
     else:
         # return the outcomes piechart for a selected site
-        fig = px.pie(filtered_df, values='class', 
+        fig = px.pie(filtered_df, values='count', 
         names='class', 
         title=f'Total Success Launch for site {entered_site}')
         return fig
